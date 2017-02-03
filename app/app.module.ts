2c75ce937e568ffe012be98eac89d8d087f54b81
Kelly Moore <a699hzz@mmm.com>
2017-02-02 18:41:00 -0600

    adding routes for dashboard

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,50 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterModule }  from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [ 
   	BrowserModule,
-  	FormsModule
+  	FormsModule,
+  	RouterModule.forRoot([
+		{
+			path: 'heroes',
+			component: HeroesComponent
+		},
+		{
+			path: 'dashboard',
+			component: DashboardComponent
+		},
+		{
+			path: '',
+			redirectTo: '/dashboard',
+			pathMatch: 'full'
+		},
+		{
+			path: 'detail/:id',
+			component: HeroDetailComponent
+		}
+	])
   ],
   declarations: [ 
   	AppComponent,
-  	HeroDetailComponent 
+  	DashboardComponent,
+  	HeroDetailComponent,
+  	HeroesComponent
+  	],
+  providers: [
+  	HeroService,
+  	{ 
+  		provide: APP_BASE_HREF, useValue: '/'
+  	}
   	],
   bootstrap:    [ AppComponent ]
 })
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,20 +1,24 @@
-import { Component, Input } from '@angular/core';
-import { Hero } from './hero';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from  '@angular/router';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/swithMap';
+
+import { HeroService } from './hero.service';
 
 @Component({
+	moduleId: module.id,
 	selector: 'my-hero-detail',
-	template: `
-	<div *ngIf="hero">
-		<h2>{{hero.name}} details!</h2>
-		<div><label>id: </label>{{hero.id}}</div>
-		<div>
-			<label>name: </label>
-			<input [(ngModel)]="hero.name" placeholder="name"/>
-		</div>
-	</div>
-	`
+	templateUrl: './hero-detail.component.html',
+	ngOnInit(): void {
+		
+	}
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
 	@Input()
 	hero: Hero;
+	constructor(
+		private heroService: HeroService,
+		private route: ActivatedRoute,
+		private location: Location
+		) {}
 }
