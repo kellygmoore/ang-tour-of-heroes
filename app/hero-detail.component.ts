3fe3248909db68cfad610596fb4f5a0f67ac3035
Kelly Moore <a699hzz@mmm.com>
2017-02-13 13:13:54 -0600

    moving parts to own files and routing

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,24 +1,36 @@
+import 'rxjs/add/operator/switchMap';
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from  '@angular/router';
 import { Location } from '@angular/common';
-import 'rxjs/add/operator/swithMap';
 
+import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
 	moduleId: module.id,
 	selector: 'my-hero-detail',
 	templateUrl: './hero-detail.component.html',
-	ngOnInit(): void {
-		
-	}
+	styleUrls: [ './hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-	@Input()
+	
 	hero: Hero;
+
 	constructor(
 		private heroService: HeroService,
 		private route: ActivatedRoute,
 		private location: Location
 		) {}
+
+	ngOnInit(): void {
+		this.route.params
+			.switchMap((params: Params) => this.heroService.getHero(+params['id']))
+			.subscribe(hero => this.hero = hero);
+	}
+
+	goBack(): void {
+		this.location.back();
+	}
 }
+
+
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@angular/core';
-
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class HeroService {
-	getHeroes(id: number): Promise<Hero[]> {
+	getHeroes(): Promise<Hero[]> {
+		return Promise.resolve(HEROES);
+	}
+
+	getHero(id: number): Promise<Hero> {
 		return this.getHeroes()
-		.then(heroes => heroes.find(hero => hero.id === id));
+			.then(heroes => heroes.find(hero => hero.id === id));
 	}
 }
